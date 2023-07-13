4c6e9fc8787c727d91f1a7d483030aa73c80ce51
vishnu.khatik <vishnu.khatik@computechcorp.com>
2023-07-13 10:59:07 +0530

    [ADD] register v-mdal with submit form in vue2 and vue3

--- src/app/auth/components/Register.vue
+++ src/app/auth/components/Register.vue
@@ -2,12 +2,23 @@
   <section class="container">
     <h1 class="large text-primary">Sign Up</h1>
     <p class="lead"><i class="fas fa-user"></i> Create Your Account</p>
-    <form class="form" action="create-profile.html">
+    <form class="form" @submit.prevent="registerForm">
       <div class="form-group">
-        <input type="text" placeholder="Name" name="name" required />
+        <input
+          type="text"
+          placeholder="Name"
+          v-model="register.name"
+          name="name"
+          required
+        />
       </div>
       <div class="form-group">
-        <input type="email" placeholder="Email Address" name="email" />
+        <input
+          type="email"
+          placeholder="Email Address"
+          name="email"
+          v-model="register.email"
+        />
         <small class="form-text"
           >This site uses Gravatar so if you want a profile image, use a
           Gravatar email</small
@@ -19,6 +30,7 @@
           placeholder="Password"
           name="password"
           minLength="6"
+          v-model="register.password"
         />
       </div>
       <div class="form-group">
@@ -27,14 +39,55 @@
           placeholder="Confirm Password"
           name="password2"
           minLength="6"
+          v-model="register.cPassword"
         />
       </div>
       <input type="submit" class="btn btn-primary" value="Register" />
     </form>
     <p class="my-1">
-      Already have an account? <a href="login.html">Sign In</a>
+      Already have an account?
+      <router-link :to="{ name: 'Login' }">Sign In</router-link>
     </p>
   </section>
 </template>
-<script setup></script>
+<!-- <script>
+import { ref } from "vue";
+export default {
+  name: "Register",
+  setup() {
+    const register = ref({
+       name: "",
+        email: "",
+        password: "",
+        cPassword: "",
+    });
+    const registerForm = () => {
+      console.log("Login", login);
+    };
+    return {
+      register,
+      registerForm,
+    };
+  },
+};
+</script> -->
+<script>
+export default {
+  data() {
+    return {
+      register: {
+        name: "",
+        email: "",
+        password: "",
+        cPassword: "",
+      },
+    };
+  },
+  methods: {
+    registerForm() {
+      console.log("registerForm", this.register);
+    },
+  },
+};
+</script>
 <style lang=""></style>
